23daf99fc75aadf4f324d38d5dc26c9fb00e1ba4
Your NamePritom003 <njahanpritom65@gmail.com>
2024-01-17 02:08:53 +0600

    completed trending products section njp

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,25 @@
 import Trendingproducts from "./HomeComponenets/trending/Trendingproducts";
+import { FaShopify } from "react-icons/fa";
 
 const Home = () => {
   return (
     <div>
       <h2>Home</h2>
-      {/* trending products pritom*/}
-      <Trendingproducts></Trendingproducts>
-      {/* ------------------------------- */}
+      {/* Trending products section */}
+      <div>
+        <h2 className="text-center font-bold mt-10 mb-5 text-3xl underline">Our trending products</h2>
+        <div>
+          <p className="text-center mb-10 p-8"> Hello Human! We are providing your essential products 
+            for your events on our website. Feel free to visit our shop and buy 
+            the best products at reasonable prices.</p>
+        </div>
+        <Trendingproducts></Trendingproducts>
+      </div>
+
+      {/* Button at the end */}
+      <div className="text-center">
+        <button className="btn btn-wide mt-4">Visit Our Shop <FaShopify /></button>
+      </div>
     </div>
   );
 };
--- client/src/pages/Home/HomeComponenets/trending/Trendingproducts.jsx
+++ client/src/pages/Home/HomeComponenets/trending/Trendingproducts.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
 import './trending.css';
-
+import Lottie from "lottie-react";
+// import shop from '../../../../assets/image/shop.webp'
+import { IoStar } from "react-icons/io5";
+import lcottievb from '../../../../../public/Animation - 1705432373781.json'
 const Trendingproducts = () => {
   const [data, setData] = useState([]);
 
@@ -11,11 +14,36 @@ const Trendingproducts = () => {
   }, []);
 
   return (
-    <div className='trending-products'>
-      {data.map((item, index) => (
-        <div key={index}>{item.itemName}</div>
-      ))}
+   <div className='flex '>
+    
+<div className=' w-72'>
+<Lottie animationData={lcottievb}></Lottie>
+</div>
+  {/* <img className=' h-72  w-96 rounded-badge' src={shop} alt="" /> */}
+     <div className='slider '>
+      <div className='slide-track gap-10'>
+        {data.map((item, index) => (
+          <div key={index} className='slide relative card
+          h-96  bg-transparent '>
+            <img className='px-4 h-24' src={item.image} alt="" />
+            <div className=' h-96 card-body  '>
+              <h2 className='card-title w-54'>{item.itemName}</h2>
+              <h2 className='font-bold w-54'> ${item.price}</h2>
+
+             <div className='flex gap-3'>
+               <h3 className=''>{item.brandName}</h3>
+             <p className='flex items-center font-bold text-blue-950 gap-2'><IoStar></IoStar> {item.rating}</p>
+             </div>
+            </div>
+
+            
+          </div>
+        ))}
+      </div>
+      
     </div>
+   
+   </div>
   );
 };
 
