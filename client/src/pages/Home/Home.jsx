69d12200a639403fc813eeadbef51cf392b97c4f
mdarrakibislamrifat <rohanrifat43@gmail.com>
2024-01-16 23:58:16 +0600

    Hot deals and footer

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,3 +1,5 @@
+import Footer from "../../components/shared/Footer";
+import HotDeals from "./HomeComponenets/HotDeals/HotDeals";
 import Trendingproducts from "./HomeComponenets/trending/Trendingproducts";
 
 const Home = () => {
@@ -6,6 +8,8 @@ const Home = () => {
       <h2>Home</h2>
       {/* trending products pritom*/}
       <Trendingproducts></Trendingproducts>
+      <HotDeals></HotDeals>
+      <Footer></Footer>
       {/* ------------------------------- */}
     </div>
   );
