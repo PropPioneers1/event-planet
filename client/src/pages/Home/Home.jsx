e6909826707bec0a3a773ac7959362eb6d7266aa
Your NamePritom003 <njahanpritom65@gmail.com>
2024-01-16 16:33:45 +0600

    pritoms first commit

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,7 +1,12 @@
+import Trendingproducts from "./HomeComponenets/trending/Trendingproducts";
+
 const Home = () => {
   return (
     <div>
       <h2>Home</h2>
+      {/* trending products pritom*/}
+      <Trendingproducts></Trendingproducts>
+      {/* ------------------------------- */}
     </div>
   );
 };
--- client/src/routes/Router.jsx
+++ client/src/routes/Router.jsx
@@ -1,10 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home/Home";
 
 const Router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
+    children:[
+      {
+        path:"/",
+        element:<Home></Home>
+    
+    
+    }
+
+    ]
   },
 ]);
 
