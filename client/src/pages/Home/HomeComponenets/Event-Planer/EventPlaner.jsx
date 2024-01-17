c330ef656c8e2a4913b8799ba09ecb4b08372878
codermahbub20 <mkrefat5@gmail.com>
2024-01-17 17:01:50 +0600

    Event planer section update

--- client/src/pages/Home/HomeComponenets/Event-Planer/EventPlaner.jsx
+++ client/src/pages/Home/HomeComponenets/Event-Planer/EventPlaner.jsx
@@ -24,35 +24,35 @@ const EventPlaner = () => {
 
 			<div className="w-full lg:w-3/4 gap-5 mb-5  mt-10 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3  mx-auto">
 				{eventPlaner?.map((event) => (
-					<div className="text-center border mt-5 mb-3 p-5 border-[#F53F7B] border-2  rounded-lg space-y-1 p-5">
-						<div className="mt-[-60px]">
-							<img
-								className="h-[150px] object-cover ml-[90px] w-[150px] border border-2 border-[#F53F7B] p-1 rounded-full"
-								src={event?.photoUrl}
-								alt=""
-							/>
-						</div>
+					<div className="text-center border relative mt-14 mb-3 p-5 border-[#F53F7B] border-2  rounded-lg space-y-1 p-5">
+						<img
+							className="h-[150px] object-cover  absolute left-1/2 -top-20 -translate-x-1/2 w-[150px] border border-2 border-[#F53F7B] p-1 rounded-full"
+							src={event?.photoUrl}
+							alt=""
+						/>
 
-						<h1 className="text-xl mt-1 font-title mt-5 font-medium">
-							{event?.eventPlannerName}
-						</h1>
-						<h3 className="text-lg text-[#F53F7B] font-title">
-							{event?.eventTitle}
-						</h3>
-						<p className="text-start mb-4  text-balance">
-							{event?.description}
-						</p>
+						<div className="pt-10">
+							<h1 className="text-xl mt-1 font-title mt-5 font-medium">
+								{event?.eventPlannerName}
+							</h1>
+							<h3 className="text-lg text-[#F53F7B] font-title">
+								{event?.eventTitle}
+							</h3>
+							<p className="text-start mb-4  text-balance">
+								{event?.description}
+							</p>
 
-						<div className="flex justify-center  items-center gap-5 ]">
-							<a href={event?.facebook}>
-								<FaFacebook className="w-7 hover:text-[#F53F7B] mt-5 h-7"></FaFacebook>
-							</a>
-							<a href="">
-								<FaLinkedin className="w-7 hover:text-[#F53F7B] mt-5 h-7"></FaLinkedin>
-							</a>
-							<a href="">
-								<FaInstagram className="w-7 hover:text-[#F53F7B] mt-5 h-7"></FaInstagram>
-							</a>
+							<div className="flex justify-center  items-center gap-5 ]">
+								<a blank href={event?.facebook}>
+									<FaFacebook className="w-7 hover:text-[#F53F7B] mt-5 h-7"></FaFacebook>
+								</a>
+								<a href="">
+									<FaLinkedin className="w-7 hover:text-[#F53F7B] mt-5 h-7"></FaLinkedin>
+								</a>
+								<a href="">
+									<FaInstagram className="w-7 hover:text-[#F53F7B] mt-5 h-7"></FaInstagram>
+								</a>
+							</div>
 						</div>
 					</div>
 				))}
