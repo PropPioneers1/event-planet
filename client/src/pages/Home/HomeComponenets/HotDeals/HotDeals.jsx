cbb3850e624220b108e899c3f1158802464fa51c
mdarrakibislamrifat <rohanrifat43@gmail.com>
2024-01-17 20:27:22 +0600

    Hot deals update

--- client/src/pages/Home/HomeComponenets/HotDeals/HotDeals.jsx
+++ client/src/pages/Home/HomeComponenets/HotDeals/HotDeals.jsx
@@ -6,12 +6,12 @@ const HotDeals = () => {
             <div  className="mt-10  flex-wrap gap-4 items-center justify-center lg:flex">
         {/* first card */}
         <div className="ml-12 lg:mr-0">
-          <div data-aos="flip-right" class="relative flex w-full max-w-[20rem] text-black flex-col rounded-xl bg-gradient-to-tr from-white to-blue-400 bg-clip-border p-8  shadow-md shadow-pink-500/40">
+          <div data-aos="flip-right" class="relative flex w-full max-w-[20rem] text-black flex-col rounded-xl bg-gradient-to-tr from-white to-pink-400 bg-clip-border p-8  shadow-md shadow-pink-500/40">
             <div class="relative m-0 mb-8 overflow-hidden rounded-none border-b border-white/10 bg-transparent bg-clip-border pb-8 text-center text-gray-700 shadow-none">
-              <p class="block font-sans text-sm font-normal uppercase leading-normal text-white antialiased">
+              <p class="block font-sans text-sm font-normal uppercase leading-normal text-black antialiased">
                 standard
               </p>
-              <h1 class="mt-6 flex justify-center gap-1 font-sans text-7xl font-normal tracking-normal text-white antialiased">
+              <h1 class="mt-6 flex justify-center gap-1 font-sans text-7xl font-normal tracking-normal text-black antialiased">
                 <span class="mt-2 text-4xl">$</span>29
                 <span class="self-end text-4xl">/mo</span>
               </h1>
@@ -81,7 +81,7 @@ const HotDeals = () => {
                     </svg>
                   </span>
                   <p class="block font-sans text-base font-normal leading-relaxed text-inherit antialiased">
-                    40+ built-in pages
+                    40+ Extra benefits
                   </p>
                 </li>
                 <li class="flex items-center gap-4">
@@ -103,7 +103,7 @@ const HotDeals = () => {
                     </svg>
                   </span>
                   <p class="block font-sans text-base font-normal leading-relaxed text-inherit antialiased">
-                    1 year free updates
+                    10 days updates
                   </p>
                 </li>
                 <li class="flex items-center gap-4">
@@ -144,12 +144,12 @@ const HotDeals = () => {
         {/* second card */}
 
         <div className="ml-12 lg:mr-0">
-        <div data-aos="flip-right" class="relative flex w-full max-w-[20rem] flex-col rounded-xl bg-gradient-to-tr from-white to-blue-400 bg-clip-border p-8 text-black shadow-md shadow-pink-500/40">
+        <div data-aos="flip-right" class="relative flex w-full max-w-[20rem] flex-col rounded-xl bg-gradient-to-tr from-white to-pink-400 bg-clip-border p-8 text-black shadow-md shadow-pink-500/40">
           <div class="relative m-0 mb-8 overflow-hidden rounded-none border-b border-white/10 bg-transparent bg-clip-border pb-8 text-center text-gray-700 shadow-none">
-            <p class="block font-sans text-sm font-normal uppercase leading-normal text-white antialiased">
+            <p class="block font-sans text-sm font-normal uppercase leading-normal text-black antialiased">
               Pro
             </p>
-            <h1 class="mt-6 flex justify-center gap-1 font-sans text-7xl font-normal tracking-normal text-white antialiased">
+            <h1 class="mt-6 flex justify-center gap-1 font-sans text-7xl font-normal tracking-normal text-black antialiased">
               <span class="mt-2 text-4xl">$</span>39
               <span class="self-end text-4xl">/mo</span>
             </h1>
@@ -175,7 +175,7 @@ const HotDeals = () => {
                   </svg>
                 </span>
                 <p class="block font-sans text-base font-normal leading-relaxed text-inherit antialiased">
-                  3 team members
+                  4 team members
                 </p>
               </li>
               <li class="flex items-center gap-4">
@@ -219,7 +219,7 @@ const HotDeals = () => {
                   </svg>
                 </span>
                 <p class="block font-sans text-base font-normal leading-relaxed text-inherit antialiased">
-                  45+ built-in pages
+                  45+ extra benefits
                 </p>
               </li>
               <li class="flex items-center gap-4">
@@ -241,7 +241,7 @@ const HotDeals = () => {
                   </svg>
                 </span>
                 <p class="block font-sans text-base font-normal leading-relaxed text-inherit antialiased">
-                  1.5 year free updates
+                  15 days updates
                 </p>
               </li>
               <li class="flex items-center gap-4">
@@ -280,13 +280,13 @@ const HotDeals = () => {
         </div>
         </div>
         <div className="ml-12 lg:mr-0">
-          <div data-aos="flip-right" class="relative flex w-full max-w-[20rem] flex-col rounded-xl bg-gradient-to-tr from-white to-blue-400 bg-clip-border p-8 text-black shadow-md shadow-pink-500/40">
+          <div data-aos="flip-right" class="relative flex w-full max-w-[20rem] flex-col rounded-xl bg-gradient-to-tr from-white to-pink-400 bg-clip-border p-8 text-black shadow-md shadow-pink-500/40">
             <div class="relative m-0 mb-8 overflow-hidden rounded-none border-b border-white/10 bg-transparent bg-clip-border pb-8 text-center text-gray-700 shadow-none">
-              <p class="block font-sans text-sm font-normal uppercase leading-normal text-white antialiased">
+              <p class="block font-sans text-sm font-normal uppercase leading-normal text-black antialiased">
                 Pro Plus
               </p>
-              <h1 class="mt-6 flex justify-center gap-1 font-sans text-7xl font-normal tracking-normal text-white antialiased">
-                <span class="mt-2 text-4xl">$</span>29
+              <h1 class="mt-6 flex justify-center gap-1 font-sans text-7xl font-normal tracking-normal text-black antialiased">
+                <span class="mt-2 text-4xl">$</span>49
                 <span class="self-end text-4xl">/mo</span>
               </h1>
             </div>
@@ -311,7 +311,7 @@ const HotDeals = () => {
                     </svg>
                   </span>
                   <p class="block font-sans text-base font-normal leading-relaxed text-inherit antialiased">
-                    3 team members
+                    6 team members
                   </p>
                 </li>
                 <li class="flex items-center gap-4">
@@ -355,7 +355,7 @@ const HotDeals = () => {
                     </svg>
                   </span>
                   <p class="block font-sans text-base font-normal leading-relaxed text-inherit antialiased">
-                    50+ built-in pages
+                    50+ extra benefits
                   </p>
                 </li>
                 <li class="flex items-center gap-4">
@@ -377,7 +377,7 @@ const HotDeals = () => {
                     </svg>
                   </span>
                   <p class="block font-sans text-base font-normal leading-relaxed text-inherit antialiased">
-                    2 years free updates
+                    1 month updates
                   </p>
                 </li>
                 <li class="flex items-center gap-4">
