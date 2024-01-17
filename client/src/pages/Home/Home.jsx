b12bab239957874e3c407fb899648442d1241fe4
anikmk <anikmollik122321@gmail.com>
2024-01-17 12:46:27 +0600

    solved conflicts

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,23 +1,18 @@
-<<<<<<< HEAD
 import UpComingEvent from "./HomeComponenets/UpComingEvent/UpComingEvent";
-=======
 import Trendingproducts from "./HomeComponenets/trending/Trendingproducts";
 import WhyChoose from "./HomeComponenets/WhyChooseEventPlanet/WhyChoose";
 import HotDeals from "./HomeComponenets/HotDeals/HotDeals";
 import Footer from "../../components/shared/Footer";
 
->>>>>>> 84eacbccc8726304cd170d16cec2267d39060074
 
 const Home = () => {
   return (
     <div>
       <h2>Home</h2>
-<<<<<<< HEAD
 
 
       {/* UpComming event anik */}
       <UpComingEvent></UpComingEvent>
-=======
       {/* trending products pritom*/}
       <Trendingproducts></Trendingproducts>
       {/* why choose by arif */}
@@ -25,7 +20,6 @@ const Home = () => {
       {/* hot deals and footer by rifat */}
       <HotDeals></HotDeals>
       <Footer></Footer>
->>>>>>> 84eacbccc8726304cd170d16cec2267d39060074
     </div>
   );
 };
