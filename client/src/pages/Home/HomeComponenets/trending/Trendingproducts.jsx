a0dfd6712ee37e9aa12a188ea88b78799ea394b6
Your NamePritom003 <njahanpritom65@gmail.com>
2024-01-17 23:57:07 +0600

    added Button in the shared folder

--- client/src/pages/Home/HomeComponenets/trending/Trendingproducts.jsx
+++ client/src/pages/Home/HomeComponenets/trending/Trendingproducts.jsx
@@ -5,6 +5,8 @@ import Lottie from "lottie-react";
 import { IoStar } from "react-icons/io5";
 import lcottievb from '../../../../../public/Animation - 1705432373781.json'
 import { FaShopify } from 'react-icons/fa';
+import Container from '../../../../components/ui/Container';
+import Buttonall from '../../../../components/ui/ButtonAll/Buttonall';
 const Trendingproducts = () => {
   const [data, setData] = useState([]);
 
@@ -15,19 +17,23 @@ const Trendingproducts = () => {
   }, []);
 
   return (
-    
-  <div className='shadow-bg'>
-     <h2 className="text-center font-bold mt-10 mb-5 text-3xl underline">Our trending products</h2>
+
+<div className='shadow-bg bg-[#EEEEEE] mt-10'>
+<Container>
+<h2 className="text-3xl font-bold text-center pt-10">Trending Products</h2>
         <div>
         <p className="text-center mb-10  p-8">
   Hello Human! We're here to make your special moments even more memorable.
   Explore our curated collection of essential products for various events and
-  find the perfect items to elevate your experiences.<br></br> Whether it's a wedding,
+  find the perfect items to elevate your experiences. Whether it's a wedding,
   celebration, or a professional gathering, our diverse range of high-quality
   products awaits you. Visit our shop now and discover a world of elegance and
   functionality at your fingertips.
 </p>
+
         </div>
+        </Container>
+      
      <div className='flex '>
     
     <div className=' w-96'>
@@ -60,13 +66,27 @@ const Trendingproducts = () => {
         </div>
        
        </div>
+  
 
-       <div className="text-center mb-10">
-        <button className="btn btn-wide mt-4
-         bg-blue-300">Visit Our Shop <FaShopify /></button>
-      </div>
+       {/* <div className="text-center mb-10  p-10">
        
+               <button className="c-button 
+                c-button--gooey  btn w-72 h-20 font-bold
+                 text-cyan-700 p-y-8 text-xl rounded-2xl"> 
+                Shop Now 
+                <FaShopify />
+  <div className="c-button__blobs">
+  <div></div>
+  <div></div>
+  <div></div>
   </div>
+</button>
+      </div> */}
+ 
+<Buttonall> Shop Now <FaShopify></FaShopify> </Buttonall>
+  </div>
+
+
   );
 };
 
