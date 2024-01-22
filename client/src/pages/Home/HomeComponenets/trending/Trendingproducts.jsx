b21d94ae95b5535664b8210cd565fc17032f1a34
Your NamePritom003 <njahanpritom65@gmail.com>
2024-01-23 01:55:48 +0600

    added smoth infinit slide

--- client/src/pages/Home/HomeComponenets/trending/Trendingproducts.jsx
+++ client/src/pages/Home/HomeComponenets/trending/Trendingproducts.jsx
@@ -1,4 +1,6 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState ,} from "react";
+import {  Link } from "react-router-dom";
+
 import "./trending.css";
 import Lottie from "lottie-react";
 // import shop from '../../../../assets/image/shop.webp'
@@ -17,20 +19,17 @@ const Trendingproducts = () => {
   }, []);
 
   return (
-    <div className="shadow-bg bg-[#EEEEEE] mt-10">
+    <div className="shadow-bg bg-blue-200 mt-10">
       <Container>
         <h2 className="text-3xl font-bold text-center pt-10">
           Trending Products
         </h2>
         <div>
-          <p className="text-center mb-10  p-8">
+          <p className="text-center mb-10 text-gray-700 text-xl p-8">
             Hello Human! We{`'`}re here to make your special moments even more
             memorable. Explore our curated collection of essential products for
             various events and find the perfect items to elevate your
-            experiences. Whether it{`'`}s a wedding, celebration, or a
-            professional gathering, our diverse range of high-quality products
-            awaits you. Visit our shop now and discover a world of elegance and
-            functionality at your fingertips.
+            experiences. 
           </p>
         </div>
         </Container>
@@ -44,21 +43,54 @@ const Trendingproducts = () => {
     
     </div>
       {/* <img className=' h-72  w-96 rounded-badge' src={shop} alt="" /> */}
-         <div className='slider rounded-r-2xl rounded-l-3xl'>
+         <div className='slider rounded-r-2xl
+          rounded-l-3xl'>
           
-          <div className='slide-track gap-10'>
-            {data.map((item, index) => (
-              <div key={index} className='slide relative card
-              h-96  bg-transparent '>
-                <img className='trendingimg px-8 h-24' src={item.image} alt="" />
-                <div className=' h-96 card-body  '>
-                  <h2 className='card-title w-54'>{item.itemName}</h2>
-                  <h2 className='font-bold w-54'> ${item.price}</h2>
-    
-                 <div className='flex gap-3'>
-                   <h3 className=''>{item.brandName}</h3>
-                 <p className='flex items-center font-bold text-blue-950 gap-2'><IoStar></IoStar> {item.rating}</p>
-                 </div>
+          <div className='slide-track gap-5 '>
+            {data.concat(data).map((item, index)=> (
+              <div key={index} className='slide relative bg-slate-200
+              card 
+              h-72 bg-transparent '>
+                  {/* <p className='flex items-center font-bold text-blue-950 gap-2'><IoStar></IoStar> {item.rating}</p> */}
+
+        
+<div className="mb-4">
+
+</div>
+
+          <div className='trendingimg w-44 relative' >
+                  <img className='h-20 w-44 mx-auto ' src={item.image} alt="" />
+                  <div className="buy-now-button absolute top-1/2 left-1/2 
+                  transform -translate-x-1/2 -translate-y-1/2 opacity-0 transition-opacity duration-300 hover:opacity-100">
+                    <button className="  border-2 backdrop-blur-lg font-bold
+                     border-slate-700 text-white px-4 py-2 rounded">Buy Now</button>
+                  </div>
+                </div>
+
+{/* 
+<Link className='trendingimg  w-96  ' to='/'>
+                <img className='h-20 w-44 mx-auto ' src={item.image} alt="" /></Link> */}
+                <div className=' h-44 w-56 '>
+                  <h2 className=' text-slate-700  text-xl  border bottom-2 border-slate-300  text-wrap font-bold mt-4 ' >{item.itemName}</h2>
+                  <h2 className='font-bold  border bottom-2 border-slate-300  text-slate-500 '>Price : <span className="text-pink-500 font-bold text-xl">${item.price}</span></h2>
+                  <h3 className='text-slate-600  border bottom-2 border-slate-300 '> Brand : {item.brandName}</h3>
+                  <div className="flex gap-2 border bottom-2 border-slate-300  ">
+            <p className="text-start  text-slate-700"> Rating :</p>
+          <div className="rating rating-sm mt-1 ">
+                  {Array.from({ length: 5 }, (_, i) => (
+                    <input
+                      key={i}
+                      type="radio"
+                      name={`rating-${item.id}`} 
+                      className={`mask mask-star-2 bg-pink-600 
+                      ${i < item.rating ? 'checked' : ''}`}
+                      checked={i < item.rating}
+                      readOnly
+                    />
+                  ))}
+                </div>
+          </div>
+
                 </div>
               </div>
             ))}
@@ -68,7 +100,7 @@ const Trendingproducts = () => {
 
   
  
-<Buttonall> Shop Now <FaShopify></FaShopify> </Buttonall>
+<Buttonall> Visit Our Shop <FaShopify></FaShopify> </Buttonall>
   </div>
 
 
