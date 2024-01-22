d3f29ba94bfaa5ca3bd83ac712e8886dc6d21c67
arifkhan101 <arifkhan230122@gmail.com>
2024-01-22 15:14:42 +0600

    [Categories]:Categories section added

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,32 +2,35 @@ import UpComingEvent from "./HomeComponenets/UpComingEvent/UpComingEvent";
 import HomeBanner from "./HomeComponenets/HomeBanner/HomeBanner";
 import Trendingproducts from "./HomeComponenets/trending/Trendingproducts";
 import WhyChoose from "./HomeComponenets/WhyChooseEventPlanet/WhyChoose";
-import HotDeals from "./HomeComponenets/HotDeals/HotDeals";
+// import HotDeals from "./HomeComponenets/HotDeals/HotDeals";
 import Footer from "../../components/shared/Footer";
 import EventPlaner from "./HomeComponenets/Event-Planer/EventPlaner";
 import TestMonial from "./HomeComponenets/Testmonial/Testmonial";
+import Categories from "./HomeComponenets/Categories/Categories";
 
 const Home = () => {
-  return (
-    <div>
-      {/* made by BRCShakil */}
-      <HomeBanner />
-      {/* UpComming event anik */}
-      <UpComingEvent></UpComingEvent>
-      {/* trending products pritom*/}
-      <Trendingproducts></Trendingproducts>
-      {/* why choose by arif */}
-      <WhyChoose></WhyChoose>
-      {/* hot deals and footer by rifat */}
-      {/* Testmonial Section */}
-      <TestMonial></TestMonial>
-      {/* <HotDeals></HotDeals> */}
-      {/* Our Event Planer */}
-      <EventPlaner></EventPlaner>
-      {/* made by Rifat */}
-      <Footer></Footer>
-    </div>
-  );
+	return (
+		<div>
+			{/* made by BRCShakil */}
+			<HomeBanner />
+			{/* Categories By Arif */}
+			<Categories></Categories>
+			{/* UpComming event anik */}
+			<UpComingEvent></UpComingEvent>
+			{/* trending products pritom*/}
+			<Trendingproducts></Trendingproducts>
+			{/* why choose by arif */}
+			<WhyChoose></WhyChoose>
+			{/* hot deals and footer by rifat */}
+			{/* Testmonial Section */}
+			<TestMonial></TestMonial>
+			{/* <HotDeals></HotDeals> */}
+			{/* Our Event Planer */}
+			<EventPlaner></EventPlaner>
+			{/* made by Rifat */}
+			<Footer></Footer>
+		</div>
+	);
 };
 
 export default Home;
---
+++ client/src/pages/Home/HomeComponenets/Categories/categoryData.js
@@ -0,0 +1,34 @@
+import { IoBookOutline } from "react-icons/io5";
+import { BsBagDash } from "react-icons/bs";
+import { HiOutlineMicrophone } from "react-icons/hi";
+import { SlLocationPin } from "react-icons/sl";
+import { MdOutlineSportsMartialArts } from "react-icons/md";
+import { MdOutlineFoodBank } from "react-icons/md";
+
+export const categories = [
+  {
+    label: 'Education',
+    icon: IoBookOutline,
+  },
+  {
+    label: 'Business',
+    icon: BsBagDash,
+  },
+  {
+    label: 'Travel',
+    icon: SlLocationPin,
+  },
+  {
+    label: 'Conference',
+    icon: HiOutlineMicrophone,
+  },
+  {
+    label: 'Food Festival',
+    icon: MdOutlineFoodBank ,
+  },
+  {
+    label: 'Sports Event',
+    icon: MdOutlineSportsMartialArts,
+  },
+  
+]
