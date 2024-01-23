f1cc2e61d5c9c91e0d9d5ddc82a3ce119cf82884
codermahbub20 <mkrefat5@gmail.com>
2024-01-24 00:57:19 +0600

    All Event Route Are Implemented

---
+++ client/src/pages/AllEvent/AllEvent.jsx
@@ -0,0 +1,78 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./AllEvent.css";
+
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import EventCard from "./EventCard";
+const AllEvent = () => {
+	return (
+		<div>
+			<div>
+				<Swiper
+					spaceBetween={30}
+					centeredSlides={true}
+					autoplay={{
+						delay: 2500,
+						disableOnInteraction: false,
+					}}
+					pagination={{
+						clickable: true,
+					}}
+					navigation={true}
+					modules={[Autoplay, Pagination, Navigation]}
+					className="mySwiper  swiper-container"
+				>
+					<SwiperSlide>
+						<img
+							className="bg-cover"
+							src="https://demos.codexcoder.com/eventterm/wp-content/uploads/2016/10/Event-00BG.jpg"
+							alt=""
+						/>
+					</SwiperSlide>
+					<SwiperSlide>
+						<img
+							src="https://ovatheme.com/em4u/wp-content/uploads/2017/09/bg_4_new.jpg"
+							alt=""
+						/>
+					</SwiperSlide>
+					<SwiperSlide>
+						<img
+							src="https://ovatheme.com/em4u/wp-content/uploads/2017/09/bg_3_new.jpg"
+							alt=""
+						/>
+					</SwiperSlide>
+					<SwiperSlide>
+						<img
+							src="https://demos.codexcoder.com/eventterm/wp-content/uploads/2016/10/Bg_0001.jpg"
+							alt=""
+						/>
+					</SwiperSlide>
+					<SwiperSlide>
+						<img
+							src="https://demos.codexcoder.com/eventterm/wp-content/uploads/2018/04/news-bg.jpg"
+							alt=""
+						/>
+					</SwiperSlide>
+					<SwiperSlide>
+						<img
+							src="https://demos.codexcoder.com/eventterm/wp-content/uploads/2018/04/event-bg.jpg"
+							alt=""
+						/>
+					</SwiperSlide>
+				</Swiper>
+			</div>
+
+			<div>
+				<EventCard></EventCard>
+				<EventCard></EventCard>
+			</div>
+		</div>
+	);
+};
+
+export default AllEvent;
--- client/src/routes/Router.jsx
+++ client/src/routes/Router.jsx
@@ -4,6 +4,7 @@ import ErrorPage from "../pages/ErrorPage/ErrorPage";
 import Home from "../pages/Home/Home";
 import SignUp from "../pages/SignUp/SignUp";
 import SignIn from "../pages/SignIn/SignIn";
+import AllEvent from "../pages/AllEvent/AllEvent";
 
 const Router = createBrowserRouter([
 	{
@@ -22,6 +23,10 @@ const Router = createBrowserRouter([
 			{
 				path: "sign-in",
 				element: <SignIn></SignIn>,
+			},
+			{
+				path: "event",
+				element: <AllEvent></AllEvent>,
 			},
 		],
 	},
