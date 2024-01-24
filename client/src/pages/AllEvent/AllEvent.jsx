629be48a2a823d7bfb4a8f6704ea132249bbd0a1
codermahbub20 <mkrefat5@gmail.com>
2024-01-24 20:36:20 +0600

    Pagination  implemented

--- client/src/pages/AllEvent/AllEvent.jsx
+++ client/src/pages/AllEvent/AllEvent.jsx
@@ -70,7 +70,6 @@ const AllEvent = () => {
 
 			<div>
 				<EventCard></EventCard>
-				<EventCard></EventCard>
 			</div>
 		</div>
 	);
--- client/src/pages/AllEvent/EventCard.jsx
+++ client/src/pages/AllEvent/EventCard.jsx
@@ -4,6 +4,10 @@ import SingleCard from "../Home/HomeComponenets/UpComingEvent/SingleCard";
 
 const EventCard = () => {
 	const [cards, setCards] = useState([]);
+
+	const [itemsPerPage, setItemsPerPage] = useState(6);
+	const [currentPage, setCurrentPage] = useState(0);
+
 	useEffect(() => {
 		fetch("./upcomingevent.json")
 			.then((res) => res.json())
@@ -12,14 +16,81 @@ const EventCard = () => {
 			});
 	}, []);
 
+	const count = cards?.length;
+	console.log(count);
+
+	const numberOfPages = Math.ceil(count / itemsPerPage);
+	const pages = [...Array(numberOfPages).keys()];
+
+	const handleItemsPerPage = (e) => {
+		const value = parseInt(e.target.value);
+		setItemsPerPage(value);
+		setCurrentPage(0);
+	};
+
+	const handlePrevPage = () => {
+		if (currentPage > 0) {
+			setCurrentPage(currentPage - 1);
+		}
+	};
+
+	const handleNextPage = () => {
+		if (currentPage < pages?.length - 1) {
+			setCurrentPage(currentPage + 1);
+		}
+	};
+
 	return (
 		<div>
 			<Container>
-				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 place-items-center py-4">
+				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 place-items-center py-10">
 					{cards &&
 						cards.map((card) => (
 							<SingleCard key={card.id} card={card}></SingleCard>
 						))}
+				</div>
+				{/* Pagination code Here */}
+				<div className="text-center py-10">
+					<button
+						className="btn btn-sm bg-primary text-white"
+						onClick={handlePrevPage}
+						style={{ marginRight: "1rem" }}
+					>
+						Prev
+					</button>
+					{pages.map((page) => (
+						<button
+							key={page}
+							onClick={() => setCurrentPage(page)}
+							style={{
+								margin: "0 0.5rem",
+								padding: "0.5rem",
+								border:
+									currentPage === page
+										? "1px solid #000"
+										: "none",
+							}}
+						>
+							{page + 1}
+						</button>
+					))}
+					<button
+						className="btn btn-sm bg-primary text-white"
+						onClick={handleNextPage}
+						style={{ marginLeft: "1rem" }}
+					>
+						Next
+					</button>
+					<select
+						className="btn btn-sm bg-primary text-white"
+						value={itemsPerPage}
+						onChange={handleItemsPerPage}
+						style={{ marginLeft: "1rem" }}
+					>
+						<option value={2}>2</option>
+						<option value={6}>6</option>
+						<option value={8}>8</option>
+					</select>
 				</div>
 			</Container>
 		</div>
