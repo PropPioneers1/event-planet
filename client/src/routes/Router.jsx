f62b6a96184b272d1f5af31b8dfffd10dd7f87ac
codermahbub20 <mkrefat5@gmail.com>
2024-01-25 00:16:09 +0600

    Test

--- client/src/routes/Router.jsx
+++ client/src/routes/Router.jsx
@@ -6,59 +6,34 @@ import SignUp from "../pages/SignUp/SignUp";
 import SignIn from "../pages/SignIn/SignIn";
 import Shopping from "../pages/Shopping/Shopping";
 
-
-import SelectWay from "../pages/CreateEvent/Chooseway.jsx/SelectWay";
-import CreateDesForm from "../pages/CreateEvent/AddEvent/CreateDesForm";
+// import SelectWay from "../pages/CreateEvent/Chooseway.jsx/SelectWay";
+// import CreateDesForm from "../pages/CreateEvent/AddEvent/CreateDesForm";
 
 const Router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      
-      {
-        path: "sign-up",
-        element: <SignUp></SignUp>,
-      },
-      {
-        path: "sign-in",
-        element: <SignIn></SignIn>,
-      },
-      {
-<<<<<<< HEAD
-        path:"shopping",
-        element:<Shopping></Shopping>
-      }
-      
-      
-    ],
-  },
-  
+	{
+		path: "/",
+		element: <MainLayout />,
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+
+			{
+				path: "sign-up",
+				element: <SignUp></SignUp>,
+			},
+			{
+				path: "sign-in",
+				element: <SignIn></SignIn>,
+			},
+			{
+				path: "shopping",
+				element: <Shopping></Shopping>,
+			},
+		],
+	},
 ]);
-=======
-        path:'select-way',
-        element:<SelectWay></SelectWay>
-
-      },
-      {
-        path:'create-form',
-        element:<CreateDesForm></CreateDesForm>
-
-      },
-       
-     	{
-				path: "event",
-				element: <AllEvent></AllEvent>,
-			}
-  
-    ],
-  },
-
->>>>>>> 77c7403aec98844ad32038d3954a324a99ab312f
 
 export default Router;
