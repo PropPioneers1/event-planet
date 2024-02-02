a381dc33b9c42caf3a2e87e257fc3bf0ab3dbf4c
codermahbub20 <mkrefat5@gmail.com>
2024-02-02 10:04:06 +0600

    Chatbot Implemented

--- client/src/pages/Home/HomeComponenets/Event-Planer/EventPlaner.jsx
+++ client/src/pages/Home/HomeComponenets/Event-Planer/EventPlaner.jsx
@@ -3,67 +3,69 @@ import { FaFacebook, FaInstagram } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 
 const EventPlaner = () => {
-  const [eventPlaner, setEventPlaner] = useState([]);
+	const [eventPlaner, setEventPlaner] = useState([]);
 
-  useEffect(() => {
-    fetch("EventPlaner.json")
-      .then((res) => res.json())
-      .then((data) => setEventPlaner(data));
-  }, []);
+	useEffect(() => {
+		fetch("EventPlaner.json")
+			.then((res) => res.json())
+			.then((data) => setEventPlaner(data));
+	}, []);
 
-  return (
-    <div className="mt-5">
-      <div className=" mx-auto md:w-6/12 lg:w-4/12 text-center my-8">
-        <h1 className="text-3xl font-title font-semibold">Our Event Planer</h1>
-        <p className="text-xl font-title border-y-2 border-[#F53F7B]  py-4 mt-2">
-          Learn more about the Planer of Event Planet
-        </p>
-      </div>
+	return (
+		<div className="mt-5">
+			<div className=" mx-auto md:w-6/12 lg:w-4/12 text-center my-8">
+				<h1 className="text-3xl font-title font-semibold">
+					Our Event Planer
+				</h1>
+				<p className="text-xl font-title border-y-2 border-[#F53F7B]  py-4 mt-2">
+					Learn more about the Planer of Event Planet
+				</p>
+			</div>
 
-      <div className="w-full lg:w-3/4 gap-5 mb-5  mt-10 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3  mx-auto">
-        {eventPlaner?.map((event, idx) => (
-          <div
-            key={idx}
-            className="text-center  relative mt-14 mb-3 border-[#F53F7B] border-2  rounded-lg space-y-1 p-5"
-          >
-            <img
-              className="h-[150px] object-cover  absolute left-1/2 -top-20 -translate-x-1/2 w-[150px]  border-2 border-[#F53F7B] p-1 rounded-full"
-              src={event?.photoUrl}
-              alt=""
-            />
+			<div className="w-full lg:w-3/4 gap-5 mb-5  mt-10 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3  mx-auto">
+				{eventPlaner?.map((event, idx) => (
+					<div
+						key={idx}
+						className="text-center  relative mt-14 mb-3 border-[#F53F7B] border-2  rounded-lg space-y-1 p-5"
+					>
+						<img
+							className="h-[150px] object-cover  absolute left-1/2 -top-20 -translate-x-1/2 w-[150px]  border-2 border-[#F53F7B] p-1 rounded-full"
+							src={event?.photoUrl}
+							alt=""
+						/>
 
-            <div className="pt-10">
-              <h1 className="text-xl mt-1 font-title font-medium">
-                {event?.eventPlannerName}
-              </h1>
-              <h3 className="text-lg text-[#F53F7B] font-title">
-                {event?.eventTitle}
-              </h3>
-              <p className="text-start mb-4  text-balance">
-                {event?.description}
-              </p>
+						<div className="pt-10">
+							<h1 className="text-xl mt-1 font-title font-medium">
+								{event?.eventPlannerName}
+							</h1>
+							<h3 className="text-lg text-[#F53F7B] font-title">
+								{event?.eventTitle}
+							</h3>
+							<p className="text-start mb-4  text-balance">
+								{event?.description}
+							</p>
 
-              <div className="flex justify-center  items-center gap-5 ]">
-                <a
-                  href={event?.facebook}
-                  target="blank"
-                  rel="noopener noreferrer"
-                >
-                  <FaFacebook className="w-7 hover:text-[#F53F7B] mt-5 h-7"></FaFacebook>
-                </a>
-                <a href="">
-                  <FaLinkedin className="w-7 hover:text-[#F53F7B] mt-5 h-7"></FaLinkedin>
-                </a>
-                <a href="">
-                  <FaInstagram className="w-7 hover:text-[#F53F7B] mt-5 h-7"></FaInstagram>
-                </a>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+							<div className="flex justify-center  items-center gap-5 ]">
+								<a
+									href={event?.facebook}
+									target="blank"
+									rel="noopener noreferrer"
+								>
+									<FaFacebook className="w-7 hover:text-[#F53F7B] mt-5 h-7"></FaFacebook>
+								</a>
+								<a href="">
+									<FaLinkedin className="w-7 hover:text-[#F53F7B] mt-5 h-7"></FaLinkedin>
+								</a>
+								<a href="">
+									<FaInstagram className="w-7 hover:text-[#F53F7B] mt-5 h-7"></FaInstagram>
+								</a>
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default EventPlaner;
