16876fdffa7047f1f6957f928282c6a52020d52e
arifkhan101 <arifkhan230122@gmail.com>
2024-02-28 12:35:51 +0600

    [taskManager]: Designed form for creating todo card

--- client/src/pages/Dashboard/ManageEvents/Tasks/Tasks.jsx
+++ client/src/pages/Dashboard/ManageEvents/Tasks/Tasks.jsx
@@ -2,15 +2,20 @@ import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../../hooks/useAuth";
 import useAxiosSecure from "../../../../hooks/useAxiosSecure";
 import { useParams } from "react-router-dom";
+import { GoPlus } from "react-icons/go";
+import { RxCross2 } from "react-icons/rx";
+import { useState } from "react";
 
 
 const Tasks = () => {
 
-    const {user} = useAuth();
+    const { user } = useAuth();
     const axiosSecure = useAxiosSecure();
-    const {id} = useParams()
+    const { id } = useParams();
 
-    const { data: board = {},  } = useQuery({
+    const [isCard, setIsCard] = useState(false)
+
+    const { data: board = {}, } = useQuery({
         queryKey: ["board", user?.email],
         queryFn: async () => {
             const result = await axiosSecure.get(
@@ -19,22 +24,68 @@ const Tasks = () => {
             return result?.data;
         },
     });
-console.log(board);
+    console.log(board);
+
+    
 
     return (
-        <div className="bg-white min-h-screen p-4">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        <div className="bg-white min-h-screen p-4 text-[#574d4d]"
+        // style={{ 
+        //     backgroundImage: `url(${board?.boardBgImg})`,
+        //     backgroundPosition: "center",
+        //     backgroundSize:"cover",
+        //     backgroundRepeat: "no-repeat",
+        //     }}
+        >
+            {/* <h2 className="text-2xl font-semibold my-6 text-center">{board?.boardName}</h2> */}
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 max-w-[1280px]">
                 {/* Todo */}
-                <div className="bg-neutral">
-                    <h2>Todo</h2>
+                <div className="bg-neutral p-4 rounded-md">
+                    <h2 className="text-xl font-semibold">{board?.boardName}: Todo</h2>
+                    {/* add a new card */}
+                    {
+                        !isCard &&
+                        <div
+                        onClick={()=>setIsCard(true)}
+                            className="flex items-center gap-2 mt-6 cursor-pointer rounded-md py-2 hover:bg-gray-300">
+                            <GoPlus size={24} />
+                            <h3 className="text-lg font-semibold">Add a card</h3>
+                        </div>
+                    }
+                    {/* Input field for add new card */}
+                    {
+                        isCard &&
+                        <div className="mt-6">
+                            <form >
+                                <textarea
+                                    placeholder="Add Todo"
+                                    className="textarea textarea-bordered min-h-[36px]  h-auto w-full focus:outline-none"
+                                ></textarea>
+                                <div className="flex items-center gap-2">
+                                    {/* Add card button */}
+                                    <button
+                                        className=" px-3 py-1 rounded-md bg-accent text-white text-lg">
+                                        Add card
+                                    </button>
+                                    {/* cancel button */}
+                                    <button
+                                    onClick={()=>setIsCard(false)}
+                                    className="btn "
+                                    >
+                                        <RxCross2 className="text-2xl" />
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    }
                 </div>
                 {/* In Progress */}
-                <div>
-                    <h1>In Progress</h1>
+                <div className="bg-neutral p-4 rounded-md">
+                    <h2 className="text-xl font-semibold">{board?.boardName}: Progress</h2>
                 </div>
                 {/* Completed */}
-                <div>
-                    <h1>Completed</h1>
+                <div className="bg-neutral p-4 rounded-md">
+                    <h2 className="text-xl font-semibold">{board?.boardName}: Completed</h2>
                 </div>
             </div>
         </div>
